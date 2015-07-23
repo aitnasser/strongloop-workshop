7f97ccb7aacf9239d513b8aeb51f46369261a994
aitnasser <aitnasser@gmail.com>
2015-07-23 09:34:55 +0100

    Signed-off-by: aitnasser <aitnasser@gmail.com>

---
+++ loopback-faq-rest-connector/local-server_wordpress/server/boot/create-sample-model.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {
+ /*  app.models.Post.create([
+    {typePost: 'Type01'}
+  ], function(err) {
+    if (err) throw err;
+    console.log('> post model created');
+  }); */
+};
